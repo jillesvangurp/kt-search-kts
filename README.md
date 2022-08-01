c89b6284fc5de160acc30f437576cb70d56e234d
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-08-01 13:44:38 +0200

    add some instructions to the readme

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 Simple  library that combines kt-search and kotlinx-cli to be able
 to create kts scripts and reuse the client initialization without 
-fuss.
+a lot of boiler-plate.
 
+## Adding kt-search-kts to your script
+
+First add the dependency:
+
+```kotlin
+@file:Repository("https://jitpack.io")
+@file:DependsOn("com.github.jillesvangurp:kt-search-kts:0.1")
+```
+
+This will transitively pull in `kt-search` and `kotlinx-cli`.
+
+Then use the `kotlinx-cli` to load the search client parameters:
+
+```kotlin
+val parser = ArgParser("script")
+val searchClientParams = parser.addClientParams()
+parser.parse(args)
+
+val client = searchClientParams.searchClient
+```
+
+See the scripts directory for a complete example.
+
+Happy scripting!
--- scripts/cluster-status.main.kts
+++ scripts/cluster-status.main.kts
@@ -1,38 +1,23 @@
 #!/usr/bin/env kotlin
 
-@file:Repository("https://repo1.maven.org/maven2")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.5")
-@file:Repository("https://maven.tryformation.com/releases")
-@file:DependsOn("com.jillesvangurp:search-client-jvm:1.99.3")
+@file:Repository("https://jitpack.io")
+@file:DependsOn("com.github.jillesvangurp:kt-search-kts:0.1")
 
-import com.jillesvangurp.ktsearch.*
+import com.jillesvangurp.ktsearch.ClusterStatus
+import com.jillesvangurp.ktsearch.clusterHealth
+import com.jillesvangurp.ktsearch.kts.addClientParams
+import com.jillesvangurp.ktsearch.kts.searchClient
+import com.jillesvangurp.ktsearch.root
 import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.default
-import kotlinx.cli.required
 import kotlinx.coroutines.runBlocking
 
 val parser = ArgParser("script")
-
-val host by parser.option(ArgType.String, shortName = "a", fullName = "host", description = "Host").default("localhost")
-val port by parser.option(ArgType.Int, shortName = "p", fullName = "port", description = "Port").default(9200)
-val user by parser.option(ArgType.String, fullName = "user", description = "Basic authentication user name if using with cloud hosting")
-val password by parser.option(ArgType.String, fullName = "password", description = "Basic authentication user name if using with cloud hosting")
-val ssl by parser.option(ArgType.Boolean, fullName = "protocol", description = "Use https if true").default(false)
+val searchClientParams = parser.addClientParams()
 parser.parse(args)
 
-val client = SearchClient(
-    // our docker test cluster runs on port 9999
-    // you may want to use 9200 with your own cluster
-    KtorRestClient(
-        host = host,
-        port = port,
-        user = user,
-        password = password,
-        https = ssl
-    )
-)
+val client = searchClientParams.searchClient
 
+// now use the client as normally in a runBlocking block
 runBlocking {
     val clusterStatus=client.clusterHealth()
     client.root().let {
